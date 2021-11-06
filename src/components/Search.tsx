9a4b6dfba1a5ac7601036c5e25b1f62e7817d686
Sheba467 <lowyouone@gmail.com>
2021-11-06 15:52:49 +0200

    adding search for stickers

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,31 +6,41 @@ import { apiKey, baseApi } from '../api';
 
 export const Search: React.FC = () => {
     const [search, setSearch] = useState('');
-    let query = 'sticker';
+    let query = search;
     let limit = 25;
-    let endPoint = `${baseApi}?api_key=${apiKey}q=${encodeURI(query)}limit=${limit}}`;
+    let rating = ["g", "pg", "pg-13", "r"];
+    let endPoint = `${baseApi}?api_key=${apiKey}&q=${query}&limit=${limit}`;
     let fetchData = fetch(endPoint)
 
- 
-    useEffect(()=>{
-        fetchData
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+    const handleSearch = ((e: any) => {
+        console.log('Search Result:');
+    })
 
 
-    }, [search])
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            fetchData
+                .then(res => res.json())
+                .then(data => console.log(data))
+                .catch(error => console.log(error))
+        }, 3000)
+
+        return () => clearTimeout(delayDebounceFn)
+    }, [fetchData, search])
+
 
     return (
         <>
-            <input type="text"
+            <input
+                type="text"
+                autoComplete='off'
                 placeholder="Enter Sticker Name"
                 onChange={(e) => setSearch(e.target.value)}
 
                 value={search}
-                onClick={()=> console.log(search)}
+
             />
-            <button onClick={()=> console.log(search) }>Search</button>
+            <button onClick={() => handleSearch(fetchData)}>Search</button>
 
 
         </>
