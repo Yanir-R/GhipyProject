0f10d53f225aaba819d999cce7ab350b1de23c52
Sheba467 <lowyouone@gmail.com>
2021-11-08 19:34:47 +0200

    add search by clicking on the Search Button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,33 +1,20 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useState } from 'react';
 import { apiKey, baseApi } from '../api';
-
-
 
 export const Search: React.FC = () => {
     const [search, setSearch] = useState('');
     let query = search;
     let limit = 25;
-    let rating = ["g", "pg", "pg-13", "r"];
+    // let rating = ["g", "pg", "pg-13", "r"];
     let endPoint = `${baseApi}?api_key=${apiKey}&q=${query}&limit=${limit}`;
     let fetchData = fetch(endPoint)
 
-    const handleSearch = ((e: any) => {
-        console.log('Search Result:');
-    })
-
-
-    useEffect(() => {
-        const delayDebounceFn = setTimeout(() => {
-            fetchData
-                .then(res => res.json())
-                .then(data => console.log(data))
-                .catch(error => console.log(error))
-        }, 3000)
-
-        return () => clearTimeout(delayDebounceFn)
-    }, [fetchData, search])
-
+    function callAPI() {
+        fetchData
+            .then(res => res.json()
+                .then(data => console.log('Giphy Stickers:', data))
+                .catch(error => console.log(error)))
+    }
 
     return (
         <>
@@ -35,14 +22,21 @@ export const Search: React.FC = () => {
                 type="text"
                 autoComplete='off'
                 placeholder="Enter Sticker Name"
+<<<<<<< HEAD
                 onChange={(e) => setSearch(e.target.value)}
 
+=======
+>>>>>>> 7d8846f... add search by clicking on the Search Button
                 value={search}
-
+                onChange={(e) => setSearch(e.target.value)}
             />
+<<<<<<< HEAD
             <button onClick={() => handleSearch(fetchData)}>Search</button>
 
 
+=======
+            <button onClick={() => callAPI()}>Search</button>
+>>>>>>> 7d8846f... add search by clicking on the Search Button
         </>
     )
 }
