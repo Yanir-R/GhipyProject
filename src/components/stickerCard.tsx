818e12493410cb63e1eee2731ea283b9694f1c96
Sheba467 <lowyouone@gmail.com>
2021-11-10 15:52:07 +0200

    created Sticker components & api call for rdm gif

--- src/components/stickerCard.tsx
+++ src/components/stickerCard.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React from 'react'
+import { Sticker } from '../react-app-env'
 
-interface stickerCardProps {
 
-}
-
-export const StickerCard: React.FC<stickerCardProps> = () => {
-
+export const StickerCard: React.FC<Sticker> = (sticker) => {
     return (
-        <div> hello</div>
-
+        <>
+            <div>
+                <div>
+                    <h1>
+                        Title
+                        {sticker.title}
+                    </h1>
+                </div>
+                <div>
+                    <h2> {sticker.id}</h2>
+                </div>
+                <div>
+                    {sticker.embed_url}
+                </div>
+            </div>
+        </>
     );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+
+
+export interface Sticker {
+    id?: string;
+    title?: string;
+    embed_url?: string;
+    type?: string
+}
